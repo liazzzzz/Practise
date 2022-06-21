54aedf2fd5190b8d957b3eb799dcf7404bffeda0
liazzzzz <ziangirovaaliya@mail.ru>
2022-06-21 08:36:36 +0300

    add changes to tasks

--- Task2/src/com/company/Main.java
+++ Task2/src/com/company/Main.java
@@ -3,21 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 4000000;
+        int max1 = 100;
+        int max2 = 4000000;
 
         int a = 1;
         int b = 2;
-        int sum = 2;
+        int sum1 = 2;
+        int sum2 = 2;
         int next = a + b;
 
-        while (next < n){
-            if (next % 2 == 0){
-                sum += next;
+        while (next < max2) {
+            if (next % 2 == 0) {
+                if (next < max1){
+                    sum1 += next;
+                }
+                sum2 += next;
             }
             a = b;
             b = next;
             next = a + b;
         }
-        System.out.println(sum);
+        System.out.println("Сумма четных членов Фиббоначи меньше 100 равна " + sum1);
+        System.out.println("Сумма четных членов Фиббоначи меньше 4000000 равна " + sum2);
     }
 }
